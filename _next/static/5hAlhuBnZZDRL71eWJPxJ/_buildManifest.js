676364363e5e19f6b0871ddb32a8eb9bc162c8e0
Ahmed Fasih <wuzzyview@gmail.com>
2022-06-15 22:56:45 -0700

    dist

---
+++ _next/static/5hAlhuBnZZDRL71eWJPxJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/595-b38675badb8ad34c.js","static/chunks/pages/index-0fa89fcbbbce7c99.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
