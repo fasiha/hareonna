174c2216e39ba49eeca7aa67403960f642a57cef
Ahmed Fasih <wuzzyview@gmail.com>
2022-06-20 18:04:18 -0700

    emoji as favico, no more 404s

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,10 @@ export default function HomePage({
     <>
       <Head>
         <title>Hareonna</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌤</text></svg>"
+        ></link>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           property="og:title"
